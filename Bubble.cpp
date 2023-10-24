2c06c33db4f911a14bd8ded17b611de9ef4e6187
Priyagoyal786 <107180659+Priyagoyal786@users.noreply.github.com>
2023-10-25 00:34:32 +0530

    Create Bubble.cpp

---
+++ Bubble.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+void bubbleSort(int arr[], int n) {
+    bool swapped;
+    for (int i = 0; i < n - 1; i++) {
+        swapped = false;
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                std::swap(arr[j], arr[j + 1]);
+                swapped = true;
+            }
+        }
+        if (!swapped) {
+            // If no elements were swapped in this pass, the array is already sorted.
+            break;
+        }
+    }
+}
+
+int main() {
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    bubbleSort(arr, n);
+
+    std::cout << "Sorted array: ";
+    for (int i = 0; i < n; i++) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
